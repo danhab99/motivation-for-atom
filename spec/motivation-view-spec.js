de2457b39e3f40f649a432496bdf7857cdd3a661
Dan Habot <dan.habot@gmail.com>
2018-05-04 22:12:25 +0300

    Created app

---
+++ spec/motivation-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MotivationView from '../lib/motivation-view';
+
+describe('MotivationView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
